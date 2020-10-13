da1187d68de4881ccd28a6bc12695b1aed7337f0
Enis Olgac II <72735869+eolgac2@users.noreply.github.com>
2020-10-13 16:51:28 +0300

    Update Hello.java

--- src/test/java/test1/Hello.java
+++ src/test/java/test1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Test");
         System.out.println("Feature - 1");
+        System.out.println("Forked by eolgac II");
     }
 }
